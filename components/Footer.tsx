5ec2bd241b69ed4dc0470a7b7385137a227d5f85
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 11:56:16 +0100

    commit on pages structure

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -63,9 +63,9 @@ const Footer: React.FC = () => {
           </span>
         </a>
       </div>
-      <span className="flex justify-center items-center py-2 space-x-4">
-        <a href="/privacy_policy" target="_blank" className="text-gray-600 hover:underline">Privacy Policy</a>
-        <a href="/terms_condition" target="_blank" className="text-gray-600 hover:underline">Terms and conditions</a>       
+      <span className="flex justify-center items-center py-6 space-x-4">
+        <a href="/privacy_policy" target="_blank" className="text-gray-600 text-base hover:underline">Privacy Policy</a>
+        <a href="/terms_condition" target="_blank" className="text-gray-600 text-base hover:underline">Terms and conditions</a>       
       </span>
 
       <div className="flex justify-center items-center py-2">
@@ -82,90 +82,4 @@ const Footer: React.FC = () => {
 export default Footer;
 
 
-
-
-
-// import React from 'react';
-// import { FaGithub, FaTwitter, FaLinkedin, FaFacebook, FaYoutube } from 'react-icons/fa';
-
-// interface FooterProps {
-//   onOpenPrivacyModal: () => void;
-//   onOpenTermsModal: () => void;
-// }
-
-// const Footer: React.FC<FooterProps> = ({ onOpenPrivacyModal, onOpenTermsModal }) => {
-//   return (
-//     <footer className="bg-secondary border-t px-6 py-4">
-//       <div className="flex justify-center space-x-2">
-//         <a
-//           href="https://github.com/sixtusdeveloper"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="border hover:transform rounded hover:scale-110"
-//         >
-//           <span className="flex items-center justify-center w-10 h-10 bg-secondary rounded-full">
-//             <FaGithub size={20} />
-//           </span>
-//         </a>
-//         <a
-//           href="https://x.com/develites01"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="border hover:transform rounded hover:scale-110"
-//         >
-//           <span className="flex items-center justify-center w-10 h-10 bg-secondary rounded-full">
-//             <FaTwitter size={20} />
-//           </span>
-//         </a>
-//         <a
-//           href="https://www.youtube.com/@Sixtusdev"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="border hover:transform rounded hover:scale-110"
-//         >
-//           <span className="flex items-center justify-center w-10 h-10 bg-secondary rounded-full">
-//             <FaYoutube size={20} />
-//           </span>
-//         </a>
-//         <a
-//           href="https://linkedin.com/in/sixtusushrey"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="border hover:transform rounded hover:scale-110"
-//         >
-//           <span className="flex items-center justify-center w-10 h-10 bg-secondary rounded-full">
-//             <FaLinkedin size={20} />
-//           </span>
-//         </a>
-//         <a
-//           href="https://web.facebook.com/develitesacademy"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//           className="border hover:transform rounded hover:scale-110"
-//         >
-//           <span className="flex items-center justify-center w-10 h-10 bg-secondary rounded-full">
-//             <FaFacebook size={20} />
-//           </span>
-//         </a>
-//       </div>
-
-//       <div className="flex justify-center space-x-4 py-2 mt-2">
-//         <a href="#" onClick={onOpenPrivacyModal} className="text-gray-600 hover:underline">
-//           Privacy Policy
-//         </a>
-//         <a href="#" onClick={onOpenTermsModal} className="text-gray-600 hover:underline">
-//           Terms of Service
-//         </a>
-//       </div>
-
-//       <div className="flex justify-center items-center py-2 mt-2">
-//         <span className="w-full flex flex-col text-center text-gray-600 text-xs p-2 tracking-wider">
-//           Copyright&copy; {new Date().getFullYear()} developed by Sixtusdev | <a href="#" onClick={onOpenTermsModal} className="text-gray-600 hover:underline">Terms and Conditions</a> | support@sixtusdev.net | version 0.1.0
-//         </span>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
 
