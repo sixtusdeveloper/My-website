ac671cc0427760ffdba3ccefc0c2906d8214ada3
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 13:34:51 +0100

    commit on Hero setion, adjusting the margin between it and navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,7 +29,7 @@ const Home = () => {
 
   return (
     <section id="portfolio" className="relative py-8 flex items-center justify-center h-screen bg-secondary">
-      <div className="relative z-10 p-8 my-16 max-w-2xl text-center">
+      <div className="relative z-10 p-8 mt-16 max-w-2xl text-center">
         <h3 className="text-[1rem] py-6 font-bold tracking-wider sm:text-[1.2rem]">
           {isSignedIn && user ? (
             <>
