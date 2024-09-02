c187111f2a7ec326c40460b05e7d0e7050c2bd2b
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 19:45:51 +0100

    commit on Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -40,7 +40,7 @@ const Home = () => {
           )}
         </h3>
         <h1 className="text-3xl lg:text-4xl leading-relaxed tracking-wide font-bold lg:font-bold">
-          I&apos;m Sixtus Aondoakaa, a Passionate Software Engineer Dedicated to Crafting Innovative Digital Solutions
+          I&apos;m Sixtus, a Passionate Software and DevOps Engineer Dedicated to Crafting Innovative Digital Solutions
         </h1>
         <p className="py-8 tracking-wide leading-6 text-[16px]">
           Crafting elegant solutions with a focus on clean, efficient code and innovative design. 
