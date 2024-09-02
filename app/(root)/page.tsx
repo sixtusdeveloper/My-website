cf3d8b1603f66dde4f574bd001127f0cd4e81335
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 03:14:10 +0100

    commit on footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -37,8 +37,6 @@ export default function Home() {
             <Projects />
             <Contact />
             <Footer 
-              onOpenPrivacyModal={handleOpenPrivacyModal} 
-              onOpenTermsModal={handleOpenTermsModal} 
             />
             <ScrollIndicator />
           </div>
