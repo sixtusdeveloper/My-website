a5ea7acef36196dd4c4b638f12482b071c2a229e
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 13:55:21 +0100

    commit on Hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   };
 
   return (
-    <section id="portfolio" className="relative py-8 flex items-center justify-center h-screen bg-secondary">
+    <section id="portfolio" className="relative py-10 flex items-center justify-center h-screen bg-secondary">
       <div className="relative z-10 p-8 mt-20 max-w-2xl text-center">
         <h3 className="text-[1rem] py-6 font-bold tracking-wider sm:text-[1.2rem]">
           {isSignedIn && user ? (
