f878873ffaded84b348e6de75ce9ba24e8b6fbd5
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-02 08:36:46 +0100

    commit on Hero section for vertical gap of y-axis

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   };
 
   return (
-    <section id="portfolio" className="relative pt-6 lg:pt-10 flex items-center justify-center h-screen bg-secondary">
+    <section id="portfolio" className="relative pt-6 lg:py-10 flex items-center justify-center h-screen bg-secondary">
       <div className="relative z-10 p-8 max-w-2xl text-center">
         <h3 className="text-[1rem] py-6 font-bold tracking-wider sm:text-[1.2rem]">
           {isSignedIn && user ? (
