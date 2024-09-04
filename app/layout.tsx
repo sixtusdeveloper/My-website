11ef11fb39572aee032df6bff18befb0ddc6ff73
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-04 12:26:59 +0100

    commit on layout.tsx file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider"; 
+import { ThemeProvider } from "@/components/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 import Head from "next/head";
 
@@ -12,30 +12,56 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <ClerkProvider
       publishableKey={publishableKey}
       appearance={{
-        variables: { 
-          colorPrimary: "#3371FF", 
-          fontSize: '16px' 
+        variables: {
+          colorPrimary: "#3371FF",
+          fontSize: '16px'
         },
         elements: {
           button: { padding: '10px 2px' },
           formFieldInput: {
-            borderRadius: '6px !important',  
-            borderColor: '#333 !important',   
-            padding: '10px !important',       
+            borderRadius: '6px !important',
+            borderColor: '#333 !important',
+            padding: '10px !important',
             backgroundColor: '#ffffff !important',
-            color: '#333 !important',        
+            color: '#333 !important',
           },
         },
       }}
     >
       <html lang="en">
         <Head>
-          <title>Sixtusdev</title>
+          <title>Sixtusdev - Innovative Solutions by Sixtus Aondoakaa</title>
           <meta name="description" content="Sixtus's personal website showcasing expertise in React, Next.js, TypeScript, and more. Explore projects, insights, and a dedication to crafting user-centric digital experiences." />
+          <meta name="keywords" content="Sixtusdev, Sixtus Aondoakaa, software engineer, React, Next.js, TypeScript, technology, projects" />
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <meta name="theme-color" content="#3371FF" />
-          <link rel="icon" href="/favicon.ico" />
           <meta name="p:domain_verify" content="a51624116ed608eb81ef7d11dd0543df" />
+
+          {/* Open Graph tags */}
+          <meta property="og:title" content="Sixtusdev - Innovative Solutions by Sixtus Aondoakaa" />
+          <meta property="og:description" content="Explore Sixtusdev, the personal website of Sixtus Aondoakaa, a skilled software engineer specializing in React, Next.js, and more. Discover innovative projects and technical insights." />
+          <meta property="og:url" content="https://sixtusdev.net" />
+          <meta property="og:image" content="https://sixtusdev.net/images/og-image.jpg" />
+          <meta property="og:type" content="website" />
+
+          {/* Structured Data */}
+          <script type="application/ld+json">
+            {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "name": "Sixtus Aondoakaa",
+              "url": "https://sixtusdev.net",
+              "description": "Sixtus Aondoakaa is a skilled software engineer specializing in React, Next.js, and more. Discover innovative projects and technical insights.",
+              "sameAs": [
+                "https://www.linkedin.com/in/sixtus-aondoakaa",
+                "https://github.com/sixtusdev"
+              ]
+            }
+            `}
+          </script>
+
+          <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
           <link
@@ -57,6 +83,78 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </ClerkProvider>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Inter } from "next/font/google";
+// import "./globals.css";
+// import { ThemeProvider } from "@/components/theme-provider"; 
+// import { ClerkProvider } from "@clerk/nextjs";
+// import Head from "next/head";
+
+// const inter = Inter({ subsets: ["latin"] });
+// const publishableKey = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+
+// export default function RootLayout({ children }: { children: React.ReactNode }) {
+//   return (
+//     <ClerkProvider
+//       publishableKey={publishableKey}
+//       appearance={{
+//         variables: { 
+//           colorPrimary: "#3371FF", 
+//           fontSize: '16px' 
+//         },
+//         elements: {
+//           button: { padding: '10px 2px' },
+//           formFieldInput: {
+//             borderRadius: '6px !important',  
+//             borderColor: '#333 !important',   
+//             padding: '10px !important',       
+//             backgroundColor: '#ffffff !important',
+//             color: '#333 !important',        
+//           },
+//         },
+//       }}
+//     >
+//       <html lang="en">
+//         <Head>
+//           <title>Sixtusdev</title>
+//           <meta name="description" content="Sixtus's personal website showcasing expertise in React, Next.js, TypeScript, and more. Explore projects, insights, and a dedication to crafting user-centric digital experiences." />
+//           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+//           <meta name="theme-color" content="#3371FF" />
+//           <link rel="icon" href="/favicon.ico" />
+//           <meta name="p:domain_verify" content="a51624116ed608eb81ef7d11dd0543df" />
+//           <link rel="preconnect" href="https://fonts.googleapis.com" />
+//           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+//           <link
+//             href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&family=Merriweather:wght@400;700&display=swap"
+//             rel="stylesheet"
+//           />
+//         </Head>
+//         <body className={inter.className}>
+//           <ThemeProvider
+//             attribute="class"
+//             defaultTheme="dark"
+//             enableSystem
+//             disableTransitionOnChange
+//           >
+//             {children}
+//           </ThemeProvider>
+//         </body>
+//       </html>
+//     </ClerkProvider>
+//   );
+// }
 
 
 
