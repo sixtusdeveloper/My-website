d70794a6781a32f707b4b6480ebd33e34ae421bb
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-04 19:03:21 +0100

    commit on portfolio page

--- app/pages/portfolio/page.tsx
+++ app/pages/portfolio/page.tsx
@@ -1,18 +1,39 @@
+'use client';
+
 import React from 'react';
-import { portfolioNavLinks } from '@/data';
-import Navbar from '@/components/Navbar';
+import Hero from '@/components/Hero';
 import Footer from '@/components/Footer';
+import { SignedIn, SignedOut, RedirectToSignIn, useUser } from "@clerk/nextjs";
+import NavBar from "@/components/Navbar";
+import { portfolioNavLinks  } from '@/data';
+import ScrollIndicator from '@/components/ScrollIndicator';
+import About from '@/components/About';
+import Blog from '@/components/Blog';
+import Testimonials from '@/components/Testimonials';
+import Projects from '@/components/Projects';
 
-const Page = () => {
-    return (
-        <div className='bg-secondary'>
-            <Navbar navigation={portfolioNavLinks} />
+export default function Home() {
 
-            <h1>Welcome to my portfolio</h1>
+  return (
+    <>
+      <SignedIn>
+        <main className="relative bg-secondary-foreground flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
+          <div className="max-w-7xl w-screen">
+            <NavBar navigation={portfolioNavLinks } />
+            <Hero />
+            <About />
+            <Blog />
+            <Testimonials />
+            <Projects />
             <Footer />
-            
-        </div>
-    );
-};
+            <ScrollIndicator />
+          </div>
+        </main>
+      </SignedIn>
 
-export default Page;
+      <SignedOut>
+        <RedirectToSignIn />
+      </SignedOut>
+    </>
+  );
+}
