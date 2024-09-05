b8f06fef3c1d92bb1a985751c831d60deb9f6a4b
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-05 10:06:52 +0100

    commit on Hero section

--- components/ui/MagicButton.tsx
+++ components/ui/MagicButton.tsx
@@ -17,11 +17,11 @@ const MagicButton = ({
 }) => {
   return (
     <button
-      className={`relative inline-flex h-12 w-full cursor-pointer overflow-hidden rounded-lg p-[1px] focus:outline-none md:w-40 ${otherClasses}`}
+      className={`relative inline-flex h-12 w-full cursor-pointer overflow-hidden rounded-lg p-[2px] focus:outline-none md:w-60 ${otherClasses}`}
       onClick={onClick}
     >
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-slate-950 px-6 text-[0.98rem] tracking-wider font-medium text-white-100 backdrop-blur-3xl gap-2">
+      <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-lg bg-secondary px-6 text-[0.98rem] tracking-wider font-medium backdrop-blur-3xl gap-2">
         {title}
       </span>
     </button>
