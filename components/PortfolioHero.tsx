ad9f81374857a22a858318ecad9738a4b688b1fe
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-05 20:35:39 +0100

    Commmit on Hero section

--- components/PortfolioHero.tsx
+++ components/PortfolioHero.tsx
@@ -51,7 +51,7 @@ const Hero = () => {
 
   return (
     <div className="bg-secondary pb-2 pt-36" id="Hero">
-      <div>
+      {/* <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
           fill="white"
@@ -65,7 +65,7 @@ const Hero = () => {
 
       <div className="h-screen w-full dark:bg-black-100 bg-secondary dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute flex items-center justify-center top-0 left-0">
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-secondary [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-      </div>
+      </div> */}
 
       <div className="flex justify-center relative my-16 lg:my-20 z-10">
         <div className="max-w-[90vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
