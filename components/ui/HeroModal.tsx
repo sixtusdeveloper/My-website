4468d5585b1e59f2fb2d27a8f5f004e2c4da5b5a
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-05 23:08:14 +0100

    Commit on HeroModal

--- components/ui/HeroModal.tsx
+++ components/ui/HeroModal.tsx
@@ -44,7 +44,7 @@ const HeroModal: React.FC<ModalProps> = ({ isOpen, onClose, message, children })
             <div className="bg-secondary border rounded-lg shadow-lg p-4 max-w-3xl mx-auto relative">
                 <button
                     onClick={onClose}
-                    className="absolute top-2 right-2 hover:text-red-600"
+                    className="absolute top-2 z-40 right-2 hover:text-red-600"
                 >
                     <IoClose size={18} />
                 </button>
