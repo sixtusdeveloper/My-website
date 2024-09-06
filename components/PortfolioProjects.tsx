49f6f2935a3fc45265b3f464c7d4c74ec453ea34
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-06 11:00:47 +0100

    commit on Project section

--- components/PortfolioProjects.tsx
+++ components/PortfolioProjects.tsx
@@ -110,10 +110,10 @@ const Projects = () => {
                 className="w-full h-3/5 object-cover transition-opacity duration-300 group-hover:opacity-75 project-img"
                 // style={{ width: 'auto', height: 'auto' }}
               />
-              <div className="px-4 my-1">
+              <div className="p-4 my-1">
                 <h1 className="font-bold text-xl truncate">{truncateTitle(title, MAX_TITLE_LENGTH)}</h1>
                 <p className="mt-2 text-sm line-clamp-2">{truncateDescription(des,  MAX_DESCRIPTION_LENGTH)}</p>
-                <div className="inline-flex gap-2 relative rounded-full border py-1 text-sm bg-transparent">
+                <div className="inline-flex w-full gap-2 relative rounded-full border px-2 py-1 text-sm bg-transparent">
                   <p className="text-sm">The magic behind?{" "}</p>
                   <a href={githubLink} target="_blank" className="font-semibold text-blue-600 cursor-pointer">
                     codebase{" "}
