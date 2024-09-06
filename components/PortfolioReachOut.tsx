5eb4ce162dcdfbec0fdc1660d6e9f6a5351a16b0
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-06 09:26:38 +0100

    commit on Reachout section

--- components/PortfolioReachOut.tsx
+++ components/PortfolioReachOut.tsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { FaEnvelope, FaWhatsapp } from 'react-icons/fa';
+// import { FaEnvelope, FaWhatsapp } from 'react-icons/fa';
 import Image from "next/image";
 
 const ReachOut = () => {
   return (
-    <div className="relative lg:pt-10 mx-auto w-full px-6 lg:px-16 pb-20" id="ReachOut">
-      <div className="max-w-4xl relative">
+    <div className="bg-secondary relative lg:pt-10 mx-auto w-full px-6 lg:px-16 pb-20" id="ReachOut">
+      <div className="max-w-4xl mx-auto relative">
         <h1 className="font-bold text-center text-[2.4rem] leading-snug tracking-wider">
           Ready to take your <span className="text-purple">digital presence</span> to the next level?
         </h1>
-        <p className='text-white-100 lg:mt-6 my-5 text-center text-md'>
+        <p className='lg:mt-6 my-5 text-center text-md'>
           Reach out to me today and let's discuss how I can help you achieve your goals. I could be more responsive on these platforms 👇
         </p>
         
         <div className="flex justify-center items-center space-x-8 mt-8">
-          <a href="mailto:sixtusushrey@gmail.com?subject=Let's%20connect!" className="text-white-100 flex items-center space-x-2 hover:text-purple">
+          <a href="mailto:sixtusushrey@gmail.com?subject=Let's%20connect!" className="flex items-center space-x-2">
               <Image 
                   src="/Gmail.png"
                   alt="Mail" 
                   width={28} 
                   height={28} 
               />
-              <span className="text-sm tracking-wide">Email</span>
+              <span className="text-base tracking-wide">Email</span>
           </a>
           
           {/* Vertical Divider */}
-          <div className="border-l border-gray-700 h-8"></div>
+          <div className="border-l h-8"></div>
           
           <a href="https://wa.me/+2349022048105" target="_blank" className="text-white-100 flex items-center space-x-2 hover:text-purple">
               <Image 
@@ -33,7 +33,7 @@ const ReachOut = () => {
                   alt="WhatsApp" 
                   width={28} 
                   height={28} />
-              <span className="text-sm tracking-wide">WhatsApp</span>
+              <span className="text-base tracking-wide">WhatsApp</span>
           </a>
         </div>
         
@@ -43,7 +43,7 @@ const ReachOut = () => {
               alt="Appreciation icon" 
               width={35} 
               height={35} />
-            <h3 className="text-center text-sm text-white-100 p-4 tacking-wide">Thank you for scrolling this far!</h3>
+            <h3 className="text-center text-base p-4 tacking-wide">Thank you for scrolling this far!</h3>
         </div>
       </div>
     </div>
