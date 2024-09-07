9469ef9cd1fd7e3f968d8eff75cd699289b9793b
sixtusdeveloper <sixtusushrey@gmail.com>
2024-09-07 23:43:08 +0100

    Commit on Auth layout.tsx file

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -35,78 +35,78 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
             <span className="text-white font-medium tracking-wide text-xl">Sixtusdev</span>
           </div>
 
-            <ul className="mt-5 space-y-4 p-8">
-                <li className="flex items-center py-2 sm:py-4">
-                    {/* <FaLaptopCode className="text-blue-600 text-xl sm:text-2xl" /> */}
-                    <Image 
-                      src="/auth01.png"
-                      alt="Laptop Code icon"
-                      width={40}
-                      height={40}
-                      style={ { width: 'auto', height: 'auto' } }
-                      className='overflow-hidden'
-                    />
-                    <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
-                    Crafting seamless and responsive user experiences on the web
-                    </span>
-                </li>
-                <li className="flex items-center py-2 sm:py-4">
-                    {/* <FaCode className="text-blue-600 text-xl sm:text-2xl" /> */}
-                    <Image 
-                      src="/auth02.png"
-                      alt="Code icon"
-                      width={40}
-                      height={40}
-                      style={ { width: 'auto', height: 'auto' } }
-                      className='overflow-hidden'
-                    />
-                    <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
-                    Engineering robust and scalable software solutions
-                    </span>
-                </li>
-                <li className="flex items-center py-2 sm:py-4">
-                    {/* <FaProjectDiagram className="text-blue-600 text-xl sm:text-2xl" /> */}
-                    <Image 
-                      src="/auth03.png"
-                      alt="Project icon"
-                      width={40}
-                      height={40}
-                      style={ { width: 'auto', height: 'auto' } }
-                      className='overflow-hidden'
-                    />
-                    <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
-                    Managing full-cycle software projects from inception to delivery
-                    </span>
-                </li>
-                <li className="flex items-center py-2 sm:py-4">
-                    {/* <FaUsers className="text-blue-600 text-xl sm:text-2xl" /> */}
-                    <Image 
-                      src="/auth04.png"
-                      alt="Collaboration icon"
-                      width={40}
-                      height={40}
-                      style={ { width: 'auto', height: 'auto' } }
-                      className='overflow-hidden'
-                    />
-                    <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
-                    Collaborating with teams to deliver impactful digital solutions
-                    </span>
-                </li>
-                <li className="flex items-center py-2 sm:py-4">
-                    {/* <FaLightbulb className="text-blue-600 text-xl sm:text-2xl" /> */}
-                    <Image 
-                      src="/auth05.png"
-                      alt="Innovation icon"
-                      width={40}
-                      height={40}
-                      style={ { width: 'auto', height: 'auto' } }
-                      className='overflow-hidden'
-                    />
-                    <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
-                    Innovating through continuous learning and creative problem-solving
-                    </span>
-                </li>
-            </ul>
+          <ul className="mt-5 space-y-4 p-2 lg:p-8">
+              <li className="flex items-center py-2 sm:py-4">
+                  {/* <FaLaptopCode className="text-blue-600 text-xl sm:text-2xl" /> */}
+                  <Image 
+                    src="/auth01.png"
+                    alt="Laptop Code icon"
+                    width={40}
+                    height={40}
+                    style={ { width: 'auto', height: 'auto' } }
+                    className='overflow-hidden'
+                  />
+                  <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
+                  Crafting seamless and responsive user experiences on the web
+                  </span>
+              </li>
+              <li className="flex items-center py-2 sm:py-4">
+                  {/* <FaCode className="text-blue-600 text-xl sm:text-2xl" /> */}
+                  <Image 
+                    src="/auth02.png"
+                    alt="Code icon"
+                    width={40}
+                    height={40}
+                    style={ { width: 'auto', height: 'auto' } }
+                    className='overflow-hidden'
+                  />
+                  <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
+                  Engineering robust and scalable software solutions
+                  </span>
+              </li>
+              <li className="flex items-center py-2 sm:py-4">
+                  {/* <FaProjectDiagram className="text-blue-600 text-xl sm:text-2xl" /> */}
+                  <Image 
+                    src="/auth03.png"
+                    alt="Project icon"
+                    width={40}
+                    height={40}
+                    style={ { width: 'auto', height: 'auto' } }
+                    className='overflow-hidden'
+                  />
+                  <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
+                  Managing full-cycle software projects from inception to delivery
+                  </span>
+              </li>
+              <li className="flex items-center py-2 sm:py-4">
+                  {/* <FaUsers className="text-blue-600 text-xl sm:text-2xl" /> */}
+                  <Image 
+                    src="/auth04.png"
+                    alt="Collaboration icon"
+                    width={40}
+                    height={40}
+                    style={ { width: 'auto', height: 'auto' } }
+                    className='overflow-hidden'
+                  />
+                  <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
+                  Collaborating with teams to deliver impactful digital solutions
+                  </span>
+              </li>
+              <li className="flex items-center py-2 sm:py-4">
+                  {/* <FaLightbulb className="text-blue-600 text-xl sm:text-2xl" /> */}
+                  <Image 
+                    src="/auth05.png"
+                    alt="Innovation icon"
+                    width={40}
+                    height={40}
+                    style={ { width: 'auto', height: 'auto' } }
+                    className='overflow-hidden'
+                  />
+                  <span className="ml-2 sm:ml-5 text-sm sm:text-base text-gray-200">
+                  Innovating through continuous learning and creative problem-solving
+                  </span>
+              </li>
+          </ul>
 
         </div>
         <div className="w-full bg-black-100 overflow-y-auto shadow-md flex items-center justify-center md:py-10 lg:py-20 p-6 sm:p-10 lg:w-1/2">
