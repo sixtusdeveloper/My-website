1c943f2372500681a342a64f7d838bfba1485ab4
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-08 15:34:30 +0100

    updated the sentrry config file

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,6 +10,13 @@ Sentry.init({
   // Add optional integrations for additional features
   integrations: [
     Sentry.replayIntegration(),
+
+    Sentry.feedbackIntegration({
+      // Additional SDK configuration goes in here, for example:
+      colorScheme: "dark",
+      isNameRequired: true,
+      isEmailRequired: true,
+    }),
   ],
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
