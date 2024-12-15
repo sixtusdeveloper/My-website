d6bae384eb3b464bad660a3ff088d2ce044f4e30
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-15 16:29:01 +0100

    updated the Portfolio services

--- components/PortfolioServices.tsx
+++ components/PortfolioServices.tsx
@@ -10,7 +10,7 @@ import Loader from '@/components/ui/Loader';
 
 
 // Truncate the Description to a maximum length
-const MAX_DESCRIPTION_LENGTH = 50;
+const MAX_DESCRIPTION_LENGTH = 55;
 const truncateDescription = (text: string, maxLength: number): string => {
   if (text.length <= maxLength) return text;
   return `${text.slice(0, maxLength)}...`;
