9eeffc82cdc561746bb4b9badd0a47744d1fe80f
sixtusdeveloper <sixtusushrey@gmail.com>
2024-12-17 08:09:52 +0100

    updated the Privacy page

--- app/pages/terms_condition/page.tsx
+++ app/pages/terms_condition/page.tsx
@@ -71,9 +71,7 @@ const TermsCondition: React.FC = () => {
                 <div className="flex items-center flex-wrap py-4">
                     <p className="text-base mt-8">You may as well read the Privacy Policy of this application <a href="https://www.sixtusdev.net/pages/privacy_policy" className="hover:text-blue-600 bg-gradient-to-r from-purple-800 via-blue-500 to-indigo-800 bg-clip-text text-transparent hover:underline">Privacy Policy</a></p>
 
-                    <div className="h-4 w-[2px] mx-2 bg-gradient-to-b from-indigo-600 via-blue-500 to-purple-800"></div>
-
-                    <p className="text-base mt-8">Go back to website <a href="https://www.sixtusdev.net/" className="hover:text-blue-600 bg-gradient-to-r from-purple-800 via-blue-500 to-indigo-800 bg-clip-text text-transparent hover:underline">Home Page</a></p>
+                    <p className="text-base mt-8">OR go back to website <a href="https://www.sixtusdev.net/" className="hover:text-blue-600 bg-gradient-to-r from-purple-800 via-blue-500 to-indigo-800 bg-clip-text text-transparent hover:underline">Home Page</a></p>
                 </div>
             </div>
         </main>    
