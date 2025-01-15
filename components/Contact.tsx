f3ee20ecafb347645a6140f3d591a3a04f34d65f
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-15 16:44:08 +0100

    updated the contact section of the website

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useState } from "react";
 import Image from "next/image";
@@ -53,7 +53,9 @@ const Contact = () => {
     }
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
   };
@@ -61,9 +63,17 @@ const Contact = () => {
   return (
     <section id="contact" className="py-20 px-4 lg:px-8 bg-secondary">
       <div className="max-w-5xl mx-auto text-center">
-        <h2 className="font-bold text-center text-[2rem] lg:text-[2.5rem] leading-snug tracking-wide mb-8"><strong>Let’s <span className='bg-gradient-to-r from-purple-800 via-blue-500 to-purple-600 bg-clip-text text-transparent'>Connect</span></strong></h2>
+        <h2 className="font-bold text-center text-[2rem] lg:text-[2.5rem] leading-snug tracking-wide mb-8">
+          <strong>
+            Let’s{" "}
+            <span className="bg-gradient-to-r from-purple-800 via-blue-500 to-purple-600 bg-clip-text text-transparent">
+              Connect
+            </span>
+          </strong>
+        </h2>
         <p className="mb-8 text-md leading-relaxed tracking-wide">
-          Feel free to reach out for collaborations, job opportunities, or just to say hi!
+          Feel free to reach out for collaborations, job opportunities, or just
+          to say hi!
         </p>
         <div className="flex flex-col md:flex-row justify-between items-start md:gap-6 lg:gap-8 gap-2">
           <div className="p-4 md:py-8 text-start relative md:w-1/2">
@@ -79,12 +89,46 @@ const Contact = () => {
               <h3 className="text-lg font-bold mt-4">Sixtus Aondoakaa</h3>
               <p className="text-base font-medium">Software Engineer</p>
               <div className="mt-4">
-                <h4 className="py-2 text-lg font-bold"><strong>Contact Me</strong></h4>
+                <h4 className="py-2 text-lg font-bold">
+                  <strong>Contact Me</strong>
+                </h4>
                 <div className="inline-flex py-1 items-center">
-                  <Image src="/phone.png" alt="" width={20} height={20} className="mr-2" /><p className="text-base py-1 mb-0">+234&nbsp;902&nbsp;2048&nbsp;105</p> 
-                </div><br />
+                  <Image
+                    src="/location.webp"
+                    alt="Location"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  <p className="text-base py-1 mb-0">
+                    Awolowo Road, Ikoyi, Lagos
+                  </p>
+                </div>
+                <br />
+                <div className="inline-flex py-1 items-center">
+                  <Image
+                    src="/phone.png"
+                    alt="Phone"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  <p className="text-base py-1 mb-0">
+                    +234&nbsp;902&nbsp;2048&nbsp;105
+                  </p>
+                </div>
+                <br />
                 <div className="inline-flex items-center">
-                  <Image src="/Gmail.png" alt="" width={20} height={20} className="mr-2" /><p className="text-base tracking-wide py-1 mb-0">contact@sixtusdev.net</p>
+                  <Image
+                    src="/Gmail.png"
+                    alt="Mail"
+                    width={20}
+                    height={20}
+                    className="mr-2"
+                  />
+                  <p className="text-base tracking-wide py-1 mb-0">
+                    contact@sixtusdev.net
+                  </p>
                 </div>
               </div>
             </div>
@@ -100,10 +144,3 @@ const Contact = () => {
 };
 
 export default Contact;
-
-
-
-
-
-
-
