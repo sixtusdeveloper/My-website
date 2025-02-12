2ee5a5ab9ab5f0e597267b836224ed02c9e0296c
sixtusdeveloper <sixtusushrey@gmail.com>
2025-02-12 22:28:54 +0100

    updated the Client component

--- components/PortfolioClients.tsx
+++ components/PortfolioClients.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Clients = () => {
   return (
     <div className="py-8 bg-secondary" id="clients">
-      <h1 className="font-extrabold font-sans text-start md:text-center text-[2rem] lg:text-[2.4rem] leading-snug tracking-wider p-8 lg:p-4 px-2">
+      <h1 className="font-extrabold font-sans text-start md:text-center text-[2rem] lg:text-[2.4rem] leading-snug tracking-wider p-8 lg:p-4">
         <strong>
           Kind words from
           <span className="bg-gradient-to-r from-yellow-600 via-blue-500 to-purple-600 bg-clip-text text-transparent">
