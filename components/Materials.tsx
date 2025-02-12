dd121661faa6d672ad1cc274c5bb18ddc31ba8db
sixtusdeveloper <sixtusushrey@gmail.com>
2025-02-12 01:33:55 +0100

    updated the Materials section - changed the colors between the 'I have logged in' and 'let me log in' buttons

--- components/Materials.tsx
+++ components/Materials.tsx
@@ -295,14 +295,14 @@ export default function CallToAction() {
               <div className="flex justify-between">
                 <button
                   onClick={handleLoggedInClick}
-                  className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md w-[48%]"
+                  className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md w-[48%]"
                 >
                   I've Logged in
                 </button>
 
                 <button
+                  className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-md w-[48%]"
                   onClick={handleLoginClick}
-                  className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md w-[48%]"
                 >
                   Let me log in
                 </button>
