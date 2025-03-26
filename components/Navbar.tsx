2d043681d3ceb65db925915ecfe2eb7fc84b4e0f
sixtusdeveloper <sixtusushrey@gmail.com>
2025-03-26 22:01:53 +0100

    updated the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -96,7 +96,7 @@ export default function Navbar({
               <Link
                 key={item.name}
                 href={`#${item.href.substring(1)}`}
-                className={`text-sm text-gray-700 dark:text-blue-100 hover:text-pink-700 tracking-wide font-semibold cursor-pointer ${
+                className={`text-sm hover:text-pink-700 tracking-wide font-semibold cursor-pointer ${
                   activeSection === item.href.substring(1)
                     ? "hover:text-pink-700 bg-gradient-to-r from-pink-600 via-pink-500 to-yellow-600 font-semibold bg-clip-text text-transparent"
                     : "text-default hover:text-pink-700"
@@ -163,7 +163,7 @@ export default function Navbar({
                 <span className="sr-only">Close menu</span>
                 <XMarkIcon
                   aria-hidden="true"
-                  className="h-6 w-6 text-gray-800 dark:text-blue-200"
+                  className="h-6 w-6 text-gray-800 dark:text-white"
                 />
               </button>
             </div>
